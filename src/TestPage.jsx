306340030889c0eff264d28bac0e41b0fa45f044
haesungJoo <haesung0728@gmail.com>
2024-11-25 17:18:01 +0900

    testpage upload

---
+++ src/TestPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestPage = () => {
+    return (
+        <div>
+            <h1>Test Page 1</h1>
+            <p>This is a test page.</p>
+        </div>
+    );
+};
+
+export default TestPage;
